4145c01daee5f2827cff79e96b744e0958cc0eb2
Sasikumar Sugumar <sasisugumar@gmail.com>
2016-10-20 20:46:42 -0400

    updated the deploy scripts
    updated the deploy scripts

--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,30 @@
 #! /bin/bash
 
+# Loop through arguments, two at a time for key and value
+while [[ $# > 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -t|--docker-tag-name)
+            DOCKER_TAG_NAME="$2"
+            shift # past argument
+            ;;
+        -r|--docker-tag-name)
+            REMOTE_TAG_NAME="$2"
+            shift # past argument
+            ;;
+        -v|--verbose)
+            VERBOSE=true
+            ;;
+        *)
+            usage
+            exit 2
+        ;;
+    esac
+    shift # past argument or value
+done
+
 # Push only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch - fixed
@@ -12,11 +37,11 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
     
     # Build and push
-    docker build -t $IMAGE_NAME .
-    echo "Pushing $IMAGE_NAME:latest"
-    docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
-    docker push "$REMOTE_IMAGE_URL:latest"
-    echo "Pushed $IMAGE_NAME:latest"
+    docker build -t $DOCKER_TAG_NAME .
+    echo "Pushing $DOCKER_TAG_NAME:latest"
+    docker tag $DOCKER_TAG_NAME:latest "$REMOTE_TAG_NAME:latest"
+    docker push "$DOCKER_TAG_NAME:latest"
+    echo "Pushed $DOCKER_TAG_NAME:latest"
   else
     echo "Skipping deploy because branch is not 'master'"
   fi

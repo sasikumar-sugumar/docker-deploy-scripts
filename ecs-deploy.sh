61ffeaef352e47f0bfe42fb8eca85ed13fce2eb4
Sasikumar Sugumar <sasisugumar@gmail.com>
2016-10-25 07:26:40 -0400

    changed the path
    changed the path

--- ecs-deploy.sh
+++ ecs-deploy.sh
@@ -5,7 +5,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
-    ./setup/bin/ecs_push.sh -c $CLUSTER -n $SERVICE -r us-east-1 -v -i $REMOTE_IMAGE_URL:latest
+    ./docker-deploy-scripts/ecs_push.sh -c $CLUSTER -n $SERVICE -r us-east-1 -v -i $REMOTE_IMAGE_URL:latest
   else
     echo "Skipping deploy because it's not an allowed branch"
   fi
